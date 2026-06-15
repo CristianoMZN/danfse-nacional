7496e068d28c2fdff745b5a410c830c67090f0b0
Cristiano Mozena <past.kun@gmail.com>
2026-06-15 17:15:11 -0300

    Adicionar testes para geração e validação de PDFs a partir de XMLs

---
+++ tests/RealXmlTest.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace DanfseNacional\Tests;
+
+use DanfseNacional\DanfseGenerator;
+use DanfseNacional\Dto\NFSe;
+use DanfseNacional\Template\DanfseTemplate;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+class RealXmlTest extends TestCase
+{
+    private string $outputDir;
+
+    protected function setUp(): void
+    {
+        $this->outputDir = __DIR__ . '/output';
+        if (!is_dir($this->outputDir)) {
+            mkdir($this->outputDir, 0755, true);
+        }
+    }
+
+    public static function realXmlProvider(): array
+    {
+        $dir = __DIR__ . '/xmls';
+        $files = glob($dir . '/*.xml');
+
+        $cases = [];
+        foreach ($files as $file) {
+            $basename = basename($file, '.xml');
+            $cases[$basename] = [$file];
+        }
+
+        return $cases;
+    }
+
+    #[DataProvider('realXmlProvider')]
+    public function test_real_xml_generates_valid_pdf(string $xmlPath): void
+    {
+        $xml = file_get_contents($xmlPath);
+        $this->assertNotFalse($xml, "Falha ao ler XML: {$xmlPath}");
+
+        $generator = new DanfseGenerator();
+        $pdf = $generator->generateFromXml($xml);
+
+        $this->assertStringStartsWith('%PDF-', $pdf, 'PDF gerado não começa com %PDF-');
+
+        $size = strlen($pdf);
+        $this->assertGreaterThan(1000, $size, 'PDF parece muito pequeno');
+        $this->assertLessThan(5_000_000, $size, 'PDF parece muito grande');
+
+        $outputPath = $this->outputDir . '/' . basename($xmlPath, '.xml') . '.pdf';
+        file_put_contents($outputPath, $pdf);
+        $this->assertFileExists($outputPath);
+    }
+
+    #[DataProvider('realXmlProvider')]
+    public function test_real_xml_parses_to_nfse_dto(string $xmlPath): void
+    {
+        $xml = file_get_contents($xmlPath);
+        $this->assertNotFalse($xml, "Falha ao ler XML: {$xmlPath}");
+
+        $generator = new DanfseGenerator();
+        $nfse = $generator->parseXml($xml);
+
+        $this->assertInstanceOf(NFSe::class, $nfse);
+        $this->assertNotNull($nfse->infNFSe, 'infNFSe não deve ser nulo');
+        $this->assertNotEmpty($nfse->infNFSe->nNFSe, 'nNFSe não deve ser vazio');
+        $this->assertNotNull($nfse->infNFSe->emit, 'emit não deve ser nulo');
+        $this->assertNotEmpty($nfse->infNFSe->emit->CNPJ, 'CNPJ do emitente não deve ser vazio');
+    }
+
+    #[DataProvider('realXmlProvider')]
+    public function test_real_xml_template_data_has_required_keys(string $xmlPath): void
+    {
+        $xml = file_get_contents($xmlPath);
+        $this->assertNotFalse($xml, "Falha ao ler XML: {$xmlPath}");
+
+        $generator = new DanfseGenerator();
+        $nfse = $generator->parseXml($xml);
+
+        $template = new DanfseTemplate();
+        $data = $template->buildData($nfse);
+
+        $this->assertArrayHasKey('chave_acesso', $data);
+        $this->assertNotEmpty($data['chave_acesso']);
+
+        $this->assertArrayHasKey('emitente', $data);
+        $this->assertArrayHasKey('nome', $data['emitente']);
+        $this->assertArrayHasKey('cnpj_cpf', $data['emitente']);
+
+        $this->assertArrayHasKey('tomador', $data);
+        $this->assertArrayHasKey('nome', $data['tomador']);
+
+        $this->assertArrayHasKey('servico', $data);
+        $this->assertArrayHasKey('descricao', $data['servico']);
+
+        $this->assertArrayHasKey('totais', $data);
+        $this->assertArrayHasKey('valor_liquido', $data['totais']);
+
+        $this->assertArrayHasKey('ambiente', $data);
+        $this->assertContains($data['ambiente'], [1, 2]);
+    }
+
+    public function test_empty_xml_throws_exception(): void
+    {
+        $this->expectException(\Throwable::class);
+
+        $generator = new DanfseGenerator();
+        $generator->generateFromXml('');
+    }
+
+    public function test_malformed_xml_throws_exception(): void
+    {
+        $this->expectException(\Throwable::class);
+
+        $generator = new DanfseGenerator();
+        @$generator->generateFromXml('<root><child>unclosed');
+    }
+
+    public function test_non_nfse_xml_generates_pdf_with_defaults(): void
+    {
+        $xml = '<?xml version="1.0" encoding="utf-8"?>'
+            . '<NFSe xmlns="http://www.sped.fazenda.gov.br/nfse" versao="1.01">'
+            . '<infNFSe Id="NFS00000000000000000000000000000000000000000000">'
+            . '<nNFSe>1</nNFSe>'
+            . '</infNFSe></NFSe>';
+
+        $generator = new DanfseGenerator();
+        $pdf = $generator->generateFromXml($xml);
+
+        $this->assertStringStartsWith('%PDF-', $pdf);
+    }
+}
